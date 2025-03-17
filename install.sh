7e7c57875fa1ce5982466388825995f67ecc5d53
George Fourtounis <gfour@admin.grnet.gr>
2025-03-17 09:24:03 +0200

    Do not run "poetry shell" during installation

--- install.sh
+++ install.sh
@@ -13,7 +13,14 @@ curl -sSL https://install.python-poetry.org | python3 -
 echo 'export PATH="/home/ubuntu/.local/bin:$PATH"' >> ~/.bash_profile
 . ~/.bash_profile
 poetry self add poetry-plugin-shell
-poetry shell
+# poetry shell
 poetry install
 
 ./setup-cert.sh $1
+
+echo
+echo "Installation complete. Run 'poetry shell' to activate the virtual "
+echo "environment, then one of the following commands:"
+echo "* 'python wallet_issuer.py' to run an issuance flow"
+echo "* 'python wallet_verifier.py' to present a credential"
+echo
